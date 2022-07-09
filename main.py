42825a42eff1a01d19e6500877b8d6025870eb0e
danprophet <76520987+danprophet@users.noreply.github.com>
2022-07-09 13:30:26 +0300

    pre-proccessing added - smooth and sharpening filter implemented and is being called from the main.py module.
    sample.png image added to image resources, and 2 result image after filter implemntation added to sub folder.

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import preprocessing
 import registration
 import verification
+import cv2
+import numpy as np
 
-print("Main logic here")
+# 0. -------- Load Image --------
+work_image = cv2.imread('image_resources/sample.png', 0) # open as gray picture
+
+# 1. -------- PreProcessing --------
+result_sharp_work_image = preprocessing.my_imfilter(work_image, preprocessing.shapreningFilter)
+cv2.imwrite('image_resources/result_filter/sharpenResult.png', result_sharp_work_image)
+result_smooth_work_image = preprocessing.my_imfilter(work_image, preprocessing.smoothFilter)
+cv2.imwrite('image_resources/result_filter/smoothResult.png', result_smooth_work_image)
+
+# 2. -------- Registration - Geometric operators --------
+
+
+# 3. -------- Verification --------
+
+
+# cv2.imshow('Original', work_image)
+# cv2.imshow('Result Sharpen', result_sharp_work_image)
+# cv2.waitKey()
--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,66 @@
 """This module handles pre processing"""
+import numpy as np
+import cv2
+
+# ----- set filters: -----
+# set sharpein Filter:
+shapreningFilter = np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]) # Laplacian, image is the function
+# shapreningFilter = np.array([[0,-1,0],[-1,4,-1],[0,-1,0]]) # Laplacian, image is the function
+# set smooth Filter:
+smoothFilter = np.array([[1,1,1],[1,1,1],[1,1,1]]) # Average of the around 1/9 * sums
+
+
+# image filtering function:
+def my_imfilter (s, filter):
+    """This function gets GRAYSCALE image s and filter, and returns image after filter implementation"""
+    filterSizeRows, filterSizeCols = filter.shape
+    imageSizeRows, imageSizeCols = s.shape
+
+    paddingDiff=filterSizeRows-1
+    resultFilter = np.zeros((imageSizeRows, imageSizeCols), dtype=np.int32)  # image to be returned at the end of process
+    resultImage = np.zeros((imageSizeRows, imageSizeCols), dtype=np.int32)  # image to be returned at the end of process
+    filterType = None  # will determine logic later
+    filterDictionary = {'sharpening':[np.array([[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]),
+                  np.array([[0,-1,0],[-1,4,-1],[0,-1,0]])], 'smoothing':[np.array([[1,1,1],[1,1,1],[1,1,1]])]}
+
+    # determing current filter, for logic to apply later
+    for filterFamily in filterDictionary.keys():
+        for currentFilter in filterDictionary[filterFamily]:
+            if np.array_equal(currentFilter, filter, equal_nan=False):
+                filterType = filterFamily
+                break
+
+    # Add padding
+    imageWithPadding = np.zeros([imageSizeRows + paddingDiff, imageSizeCols + paddingDiff], dtype=np.uint8)
+    for i in range(imageSizeRows):
+        for j in range(imageSizeCols):
+            imageWithPadding[i+int(paddingDiff/2), j+int(paddingDiff/2)] = s[i,j]
+
+    # implement the filter:
+    for i in range(imageSizeRows):
+        for j in range(imageSizeCols):
+            pixelCalculation = 0  # for filter calculation later
+            # calculate each pixel in the image after filter
+            for filterRow in range(filterSizeRows):
+                for filterCol in range(filterSizeCols):
+                    pixelCalculation += imageWithPadding[i+filterRow, j+filterCol] \
+                                        * filter[filterRow, filterCol]
+
+            if filterType == 'smoothing':
+                resultFilter[i, j] = int(pixelCalculation / (filterSizeRows * filterSizeCols))
+            elif filterType == 'sharpening':
+                resultFilter[i, j] = pixelCalculation
+
+    if filterType == 'smoothing':
+        resultFilter=resultFilter.astype(dtype=np.uint8)
+        return resultFilter  # end logic for smoothing filter, image is ready
+
+    if filterType == 'sharpening':
+        resultFilter = resultFilter * 100.0 / (np.amax(resultFilter)-np.amin(resultFilter)) # normalize
+        resultFilter = resultFilter.astype(np.int32)
+        resultImage = s + resultFilter # add filter to original image
+        resultImage = abs(resultImage) # drop negative values
+        resultImage = resultImage* 255.0 / np.amax(resultImage) # normalize back to 256 scale
+        resultImage = resultImage.astype(np.uint8)
+        return resultImage
+
