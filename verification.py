89421decd87cd47b744a5babef1c83050d1672c0
danprophet <76520987+danprophet@users.noreply.github.com>
2022-07-11 01:32:24 +0300

    morphological_operators - special cases for borders, not finished yet

--- verification.py
+++ verification.py
@@ -26,13 +26,57 @@ def get_thershold(image, thrshold=0.7):
 
 
 def morphological_operators(binary_image, se, se_center=[0, 0]):
+    """This function executes the morphological_operators according to the passed stracture element se.
+    se_center = [x_pos, y_pos] coordinates of the se
+    se = stracture element
+    binary_image = passed binary image (thresholded)
+    """
     cols, rows = binary_image.shape
     se_cols, se_rows = se.shape
     center_value = se[se_center[0], se_center[1]]
-    result = np.zeros([rows, cols], dtype=np.bool)
+    result = np.zeros([rows+(2*se_rows), cols+(2*se_cols)], dtype=np.bool)  # result with pad
 
-    for i in range (rows):
+    for i in range(rows):
         for j in range(cols):
+            se_forward_distance_row = ((se_rows-1) - se_center[0])
+            se_forward_distance_col = ((se_cols-1) - se_center[1])
+
+            # edge cases:
+            # 1. 4 edges
+            if (i-se_center[0] < 0) and (j-se_center[1] < 0):  # top left corner
+                sliced_se = se[se_center[0]:, se_center[1]:]
+                sliced_binary = binary_image[i:se_rows - se_center[0], j:se_cols - se_center[1]]
+
+            elif (i+se_forward_distance_row >= rows) and (j+se_forward_distance_col >= cols):  # bottom right corner
+                se_row_diff = i + se_forward_distance_row - (rows-1)  # row se pixels out of border
+                se_col_diff = j + se_forward_distance_col - (cols-1)  # col se pixels out of border
+                sliced_se = se[se_center[0]:se_rows - se_row_diff, se_center[1]:se_cols - se_col_diff]
+                sliced_binary = binary_image[i:, j:]
+
+            elif (i-se_center[0] < 0) and (j + se_forward_distance_col >= cols):  # top right corner
+                se_col_diff = j + se_forward_distance_col - (cols-1)  # col se pixels out of border
+                sliced_se = se[se_center[0]:, se_center[1]:se_cols - se_col_diff]
+                sliced_binary = binary_image[i:se_rows - se_center[0], j:]
+
+            elif (i+se_forward_distance_row >= rows) and (j-se_center[1] < 0):  # bottom left corner
+                se_row_diff = i + se_forward_distance_row - (rows-1)  # row se pixels out of border
+                sliced_se = se[se_center[0]:se_rows - se_row_diff, se_center[1]:]
+                sliced_binary = binary_image[i:, j:se_cols - se_center[1]]
+
+            # 2. just borders
+            elif i-se_center[0] < 0:  # only rows upper part
+                sliced_se = se[se_center[0]:, :]
+                sliced_binary = binary_image[i:se_rows - se_center[0], j - se_center[1]:j + se_forward_distance_col+1]
+
+            elif j-se_center[1] < 0:  # only cols left part
+                pass
+            elif i+se_forward_distance_row >= rows:  # only rows bottom part
+                pass
+            elif j + se_forward_distance_col >= cols:  # only rows bottom part
+                pass
+            else:  # middle matrix
+                pass
+
             try:
                 if binary_image[i, j] == center_value:  # found matching value
                     startPosRow = i - se_center[0]
